86dffe3cc7823974433bb678d9117c8ebca82610
wioenena <wioenena@gmail.com>
2026-01-18 15:22:08 +0300

    chore: removed for re-implementation

--- libs/dinord/gateway/EventHandler.ts
+++
@@ -1,9 +0,0 @@
-import type { Client } from "../Client.ts";
-import type { GatewayReceiveEventData } from "./types.d.ts";
-
-export abstract class EventHandler<Payload> {
-	public abstract handle(
-		client: Client,
-		data: GatewayReceiveEventData<Payload>,
-	): Promise<void> | void;
-}
