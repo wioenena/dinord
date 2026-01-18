b51f453402f21d745672585086bd773041998931
wioenena <wioenena@gmail.com>
2026-01-19 01:51:52 +0300

    feat: defined and renamed some types

--- libs/dinord/shard/types.d.ts
+++ libs/dinord/shard/types.d.ts
@@ -1,7 +1,15 @@
+import type { Nullable } from "../types.d.ts";
+import type { GatewayOpCode } from "./constants.ts";
+
 export type ShardId = number;
 
-export type ShardOptionsProps = {
+export type ShardManagerOptions = {
   totalShards: number | "auto";
+  token: string;
+  compress?: boolean;
+  largeThreshold?: number;
+  presence?: never; // TODO: create type for presence
+  intents: number;
 };
 
 export type GatewayBotResponse = {
@@ -13,4 +21,34 @@ export type GatewayBotResponse = {
     reset_after: number;
     max_concurrency: number;
   };
+};
+
+export type GatewayReceiveEventPayload<T = unknown> = {
+  op: GatewayOpCode;
+  d: Nullable<T>;
+  s: Nullable<number>;
+  t: Nullable<string>;
+};
+
+export type GatewaySendEventPayload<T = unknown> = {
+  op: GatewayOpCode;
+  d: Nullable<T>;
+};
+
+export type HelloEventPayload = {
+  heartbeat_interval: number;
+};
+
+export type IdentifyPayload = {
+  token: string;
+  properties: {
+    os: string;
+    browser: string;
+    device: string;
+  };
+  compress?: boolean;
+  large_threshold?: number;
+  shard?: [number, number];
+  presence?: never; // TODO: create type for presence
+  intents: number;
 };
