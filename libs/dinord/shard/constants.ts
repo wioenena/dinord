e54d3de97653fdaad184f8b7bcdae14b11b74bbb
wioenena <wioenena@gmail.com>
2026-01-19 01:51:40 +0300

    feat: defined gateway opcodes

--- libs/dinord/shard/constants.ts
+++ libs/dinord/shard/constants.ts
@@ -1,1 +1,16 @@
 export const WS_URL = "wss://gateway.discord.gg/?v=10&encoding=json";
+
+export enum GatewayOpCode {
+  Dispatch,
+  Heartbeat,
+  Identify,
+  PresenceUpdate,
+  VoiceStateUpdate,
+  Resume = 6,
+  Reconnect,
+  RequestGuildMembers,
+  InvalidSession,
+  Hello,
+  HeartbeatACK,
+  RequestSoundboardSounds = 31,
+}
