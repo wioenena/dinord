3c717b2c98e49b5d1ec6d5dceade75e2be579380
wioenena <wioenena@gmail.com>
2026-01-19 01:50:37 +0300

    refactor: removed client options and managers added to constructor

--- libs/dinord/client/Client.ts
+++ libs/dinord/client/Client.ts
@@ -1,24 +1,17 @@
-import { Logger } from "../logger/mod.ts";
-import { RestManager } from "../rest/mod.ts";
-import { ShardManager } from "../shard/mod.ts";
+import { Logger, type LogLevel } from "../logger/mod.ts";
+import type { RestManager } from "../rest/mod.ts";
+import type { ShardManager } from "../shard/mod.ts";
 import { defProp } from "../utils/mod.ts";
-import type { ClientOptions } from "./ClientOptions.ts";
 
 export class Client {
-  public readonly options!: ClientOptions;
   public readonly logger!: Logger;
   public readonly restManager!: RestManager;
   public readonly shardManager!: ShardManager;
 
-  public constructor(options: ClientOptions) {
-    defProp(this, "options", options);
-    defProp(this, "logger", new Logger(options.logLevel, Client.name));
-    defProp(this, "restManager", new RestManager(this.logger.level, this.options.token));
-    defProp(
-      this,
-      "shardManager",
-      new ShardManager(this.restManager, this.options.shardOptions, this.logger.level),
-    );
+  public constructor(restManager: RestManager, shardManager: ShardManager, logLevel: LogLevel) {
+    defProp(this, "logger", new Logger(logLevel, Client.name));
+    defProp(this, "restManager", restManager);
+    defProp(this, "shardManager", shardManager);
   }
 
   public async start(): Promise<void> {
