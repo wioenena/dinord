88fcf0ceb83a542259b09d7c6fc42d2216a1282c
wioenena <wioenena@gmail.com>
2026-01-20 15:43:22 +0300

    feat: image size supported for cdn

--- libs/api-types/cdn/types.d.ts
+++ libs/api-types/cdn/types.d.ts
@@ -1,2 +1,3 @@
 export type ImageFormat = ".jpg" | ".jpeg" | ".png" | ".webp" | ".gif" | ".avif" | ".json";
+export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
 export type ValidateImageFormat<T extends ImageFormat> = T;
