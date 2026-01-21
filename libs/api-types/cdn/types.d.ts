e791a5353d7062b5d08f1113541c4a468e4742f2
wioenena <wioenena@gmail.com>
2026-01-21 13:06:31 +0300

    feat: removed ValidateImageFormat utility type and ImageSize type supported strings now

--- libs/api-types/cdn/types.d.ts
+++ libs/api-types/cdn/types.d.ts
@@ -1,3 +1,20 @@
 export type ImageFormat = ".jpg" | ".jpeg" | ".png" | ".webp" | ".gif" | ".avif" | ".json";
-export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
-export type ValidateImageFormat<T extends ImageFormat> = T;
+export type ImageSize =
+  | 16
+  | 32
+  | 64
+  | 128
+  | 256
+  | 512
+  | 1024
+  | 2048
+  | 4096
+  | "16"
+  | "32"
+  | "64"
+  | "128"
+  | "256"
+  | "512"
+  | "1024"
+  | "2048"
+  | "4096";
