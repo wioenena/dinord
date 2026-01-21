3f00930ecaaa3fc975b0bab3200ea92ebeb2086f
wioenena <wioenena@gmail.com>
2026-01-21 13:59:56 +0300

    refactor: removed string support for image sizes

--- libs/api/cdn/types.d.ts
+++ libs/api/cdn/types.d.ts
@@ -1,20 +1,2 @@
 export type ImageFormat = ".jpg" | ".jpeg" | ".png" | ".webp" | ".gif" | ".avif" | ".json";
-export type ImageSize =
-  | 16
-  | 32
-  | 64
-  | 128
-  | 256
-  | 512
-  | 1024
-  | 2048
-  | 4096
-  | "16"
-  | "32"
-  | "64"
-  | "128"
-  | "256"
-  | "512"
-  | "1024"
-  | "2048"
-  | "4096";
+export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
